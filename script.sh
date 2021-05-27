214cf9b320af7b5e112bf822bb4754a3f3b681e1
Diego Alberto <dmalberto@outlook.com>
2021-05-26 23:45:45 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go build phishingkiller.go
+./phishingkiller http://gerencianet.servicoespecial.com/assinatura.php
